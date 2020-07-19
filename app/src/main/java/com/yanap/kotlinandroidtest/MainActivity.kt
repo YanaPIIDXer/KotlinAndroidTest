7e1311cc46729711e6fca555a42f05a337eee4ec
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-19 12:31:49 +0900

    テキストをソースコード上で書き換えるテスト。

--- app/src/main/java/com/yanap/kotlinandroidtest/MainActivity.kt
+++ app/src/main/java/com/yanap/kotlinandroidtest/MainActivity.kt
@@ -2,10 +2,23 @@ package com.yanap.kotlinandroidtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
+// アクティビティ
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 文字列をブチ込むテスト
+        val textView = findViewById(R.id.textView) as TextView
+        // ↓リテラル直書きは推奨されないらしい。
+        //textView.text = "Fucking Text"
+        //textView.setText("Fucking Text")
+        // ↓変数噛ませると・・・？
+        val fuckingText = "Fucking Variable Text"
+        textView.setText(fuckingText)
+        // ↓Resourceに放り込んでおいたものを引っ張ってくる。
+        //textView.setText(R.string.FuckingText)
     }
 }
