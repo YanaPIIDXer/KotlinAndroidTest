51f5eee91447922afb5b2ef53de21cc4d37d9b9f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-19 12:52:08 +0900

    activity_mainとは別のレイアウトを定義。

--- app/src/main/java/com/yanap/kotlinandroidtest/MainActivity.kt
+++ app/src/main/java/com/yanap/kotlinandroidtest/MainActivity.kt
@@ -8,17 +8,6 @@ import android.widget.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // 文字列をブチ込むテスト
-        val textView = findViewById(R.id.textView) as TextView
-        // ↓リテラル直書きは推奨されないらしい。
-        //textView.text = "Fucking Text"
-        //textView.setText("Fucking Text")
-        // ↓変数を一旦挿むか、
-        val fuckingText = "Fucking Variable Text"
-        textView.text = fuckingText
-        // ↓あらかじめResourceに放り込んでおいたものを引っ張ってくる。
-        //textView.setText(R.string.FuckingText)
+        setContentView(R.layout.activity_sub)
     }
 }
